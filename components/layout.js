fb70a7954cbadf55d3cda722118e9bfdd638ba61
Taro Yamauchi <tyamauchi@medibang.com>
2023-05-01 15:21:23 +0900

    test

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <meta name="description" content="TechnologyとConvenienceを組み合わせた造語。​ITがもたらす便利なものを紹介します。最近はAI関連の記事が多いです。ChatGPT / Google Bard / OpenAI GPT / Replika" />
+        <meta name="description" content="TechnologyとConvenienceを組み合わせた造語。​ChatGPTなどのAI技術がもたらす便利なものを紹介します。" />
       </Head>
       <Navbar />
       <main>{children}</main>
